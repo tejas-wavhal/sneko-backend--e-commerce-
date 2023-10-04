19e2cfd6f1148ee1bd37935afe0d56a3241c77fa
Tejas <twavhal7514@gmail.com>
2023-10-05 01:31:59 +0530

    some changes

--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,7 @@
         config: {
           provider: 'cloudinary',
           providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
+            cloud_name: env('CLOUDINARY_NAME'), 
             api_key: env('CLOUDINARY_KEY'),
             api_secret: env('CLOUDINARY_SECRET'),
           },
